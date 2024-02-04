45613b80e787c23215fb45cb722d85e39ec92213
FahimaChowdhury <fahima.chowdhury.173@gmail.com>
2024-02-04 20:40:43 +0600

    Add number parser

---
+++ src/include/AST.h
@@ -0,0 +1,203 @@
+//===- AST.h - Node definition for the Kelidoscop AST ----------------------------===//
+//
+// This file implements the AST for the Kelidoscop language. It is optimized for
+// simplicity, not efficiency. The AST forms a tree structure where each node
+// references its children using std::unique_ptr<>.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LEXER_H
+#define LEXER_H
+
+
+#include "Lexer.h"
+#include <vector>
+#include <memory>
+
+
+/// Here the type of inheritance used: Heirarchical
+
+/// This is base class
+class ExprAst {
+
+public:
+
+    ExprAst() {
+
+        printf("ExprAst class\n");
+    }
+
+    /// Declare Destructor
+    /// Why virtual???
+    /// To achieve dynamic polymorphism. Which is the ability to call Derived class function using Base class pointer or reference
+    /// By using virtual destructor in Base class we are making sure that we first call Derived class destructor and 
+    /// then Base class destructor for the case - calling Derived class function using Base class pointer or reference
+    virtual ~ExprAst() {} 
+    virtual double getVal() const { throw std::runtime_error("getValue() not supported by this node type"); }
+};
+
+
+
+/// Derived class
+class NumberExprAst : public ExprAst {
+
+    double Val;
+
+public:
+
+    /// Declare Constructor
+    /// double v is the constructore argument taking input
+    /// : val(v) means member initializer list. Used to initialize class member which is val in here.
+    NumberExprAst(double V) : 
+                  Val(V) {
+
+        printf("NumberExprAst class\n");
+    
+    } /// need to keep body empty
+
+
+    double getVal() const { return Val; }
+};
+
+
+
+/// Derived class
+class VariableExprAst : public ExprAst {
+
+    std::string Name; 
+
+public:
+
+    /// Declare Constructor
+    /// const std::string &n means reference parameter which is const and unchangable
+    /// we will be not getting copy of the string but the string itself by referencing or using alias n
+    /// : Name(N) this is member initializer list. Used to initialize class member which is Name in here.
+    VariableExprAst(const std::string &N) : 
+                    Name(N) {
+
+        printf("VariableExprAst class\n");
+
+    } ///need to keep body empty
+
+};
+
+
+/// Derived class
+class BinaryExprAst : public ExprAst {
+
+    char Optr;  /// + - * / < >
+
+    /// Creating 2 smart pointers capable of owning and managing objects of type ExprAst
+    std::unique_ptr<ExprAst> LHS, RHS;
+
+public:
+
+    /// Declare constructor
+    /// To initialize a BinaryExprAst object with a specific operator 
+    /// and left-hand side (LHS) and right-hand side (RHS) expressions.
+    BinaryExprAst(char Op, std::unique_ptr<ExprAst> LHS, std::unique_ptr<ExprAst> RHS) : 
+                  Optr(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {      /// move: transfering the ownership of LHS came from 
+                                                                            /// outside of the class to LHS inside of the class
+        printf("BinaryExprAst class\n");
+
+    }
+
+};
+
+
+/// Derived class
+class CallExprAst : public ExprAst {
+
+    std::string Callee;
+
+    /// Its a vector (dynamic array) that holds multiple smart pointers. 
+    /// Each of these smart pointers is of type std::unique_ptr<ExprAst>, meaning each one uniquely owns an ExprAst object.
+    std::vector<std::unique_ptr<ExprAst>> Args;
+
+public:
+
+    CallExprAst(const std::string &C, std::vector<std::unique_ptr<ExprAst>> Args) : 
+                Callee(C), Args(std::move(Args)) {
+
+        printf("CallExprAst class\n");
+
+    }
+
+};
+
+
+/// Another Base class
+/// PrototypeAst - This class represents the "prototype" for a function,
+/// which captures its name, and its argument names (thus implicitly the number
+/// of arguments the function takes).
+class PrototypeAst {
+  
+    std::string Name;
+
+    
+    std::vector<std::string> Args;
+
+public:
+    PrototypeAst(const std::string &N, std::vector<std::string> Args):  
+                 Name(N), Args(std::move(Args)) {
+
+        printf("PrototypeAst class\n");
+
+    }
+
+
+    const std::string &getName() const { return Name; }
+
+};
+
+
+/// Another Base class
+/// FunctionAst - This class represents a function definition itself.
+class FunctionAst {
+
+    std::unique_ptr<PrototypeAst> Proto;
+    std::unique_ptr<ExprAst> Body;
+
+public:
+    FunctionAst(std::unique_ptr<PrototypeAst> Proto, std::unique_ptr<ExprAst> Body) : 
+                Proto(std::move(Proto)), Body(std::move(Body)) {
+
+        printf("FunctionAst class\n");
+
+  }
+
+};
+
+
+
+#endif /// LEXER_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/toy-compiler.cpp
@@ -0,0 +1,147 @@
+//===- toyc.cpp - The Kelidoscop Compiler ----------------------------------------===//
+//
+// This file implements the entry point for the Kelidoscop compiler.
+//
+//===----------------------------------------------------------------------===//
+
+
+// "Parser.h" is calling "AST.h"
+// "AST.h" is calling "Lexer.h"
+// "Lexer.h" is top and calling no one.
+
+
+#ifndef INCLUDE_PARSER_H
+#define INCLUDE_PARSER_H
+
+
+#include "include/Parser.h"
+
+
+
+
+
+int main() {
+
+    fprintf(stderr, "ready> ");
+
+    getNextToken();
+
+    MainLoop();
+
+    return 0;
+
+
+}
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /// our compiler take an input filename (i.e. filename.toy).
+// static cl::opt<std::string> inputFilename(
+//     cl::Positional,
+//     cl::desc("<input toy file>"),
+//     cl::init("-"),
+//     cl::value_desc("filename")
+// );
+
+
+// namespace {
+// enum Action { None, DumpAST };
+// } // namespace
+
+// /// This declaration defines a variable “emitAction” of the “Action” enum type.
+// /// For more - https://llvm.org/docs/CommandLine.html#selecting-an-alternative-from-a-set-of-possibilities
+// static cl::opt<enum Action> emitAction(
+//     "emit",
+//     cl::desc("Select the kind of output desired"),
+//     cl::values(
+//         clEnumValN(DumpAST, "ast", "output the AST dump")
+//     )
+// );
+
+
+// /// Returns a Toy AST resulting from parsing the file or a nullptr on error.
+// std::unique_ptr<toy::ModuleAST> parseInputFile(llvm::StringRef filename) {
+    
+//     llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> fileOrErr =
+//         llvm::MemoryBuffer::getFileOrSTDIN(filename);
+    
+//     if (std::error_code ec = fileOrErr.getError()) {
+//         llvm::errs() << "Could not open input file: " << ec.message() << "\n";
+//         return nullptr;
+//     }
+
+//     auto buffer = fileOrErr.get()->getBuffer();
+//     LexerBuffer lexer(buffer.begin(), buffer.end(), std::string(filename));
+//     Parser parser(lexer);
+    
+//     return parser.parseModule();
+// }
+
+
+
+
+// int main(int argc, char **argv) {
+
+//     // Parse the command line arguments & flags
+//     cl::ParseCommandLineOptions(argc, argv, "toy compiler\n");
+    
+//     // Parse code file (i.e. code into LLVM module)
+//     // Lexer & Parser are called inside
+//     auto moduleAST = parseInputFile(inputFilename);
+    
+
+//     if (!moduleAST)
+//         return 1;
+
+
+//     switch (emitAction) {
+
+//         // Then Dump the AST, if -emit=ast argument is passed
+//         case Action::DumpAST:
+//             dump(*moduleAST);
+//             return 0;
+        
+//         default:
+//             llvm::errs() << "No action specified (parsing only?), use -emit=<action>\n";
+//     }
+
+//     return 0;
+// }
